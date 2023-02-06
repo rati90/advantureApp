1e02b7f7ec8f6f03c2164ec114967d80df40eca7
ratibakhtadze <ratibakhtadze@gmail.com>
2023-02-06 13:27:31 +0400

    adventure post and models added

--- services/backend/models.py
+++ services/backend/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 from pydantic.networks import EmailStr
 
 
@@ -6,7 +6,28 @@ class User(BaseModel):
     email: EmailStr
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+class Item(BaseModel):
+    name: str = Field(max_length=50)
+    description: str = Field(max_length=250)
+    price: float = None
+    tax: float | None = None
+    image: Image | None = None
+
+
 class UserProfile(User):
     name: str = Field(min_length=1, max_length=50, regex="[a-zA-Z]+")
     surname: str = Field(min_length=1, max_length=50, regex="[a-zA-Z]+")
     birthdate: str
+    image: Image | None = None
+
+
+class Adventure(BaseModel):
+    adventure_id = str
+    name: str = Field(max_length=50)
+    description: str = Field(max_length=250)
+    image: Image | None = None
+    items: list[Item]
--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from services.backend.models import UserProfile
+from services.backend.models import UserProfile, Adventure
 
 
 app = FastAPI()
@@ -11,8 +11,15 @@ async def get_home():
     return {"home_page": "Welcome"}
 
 
-@app.get("/user_profile/")
+@app.post("/user_profile/")
 async def get_profile(user_profile: UserProfile):
     return {"Name": user_profile.name,
             "Surname": user_profile.surname,
             "Birthdate": user_profile.birthdate}
+
+
+@app.post("/adventure/{adventure_id}")
+async def get_adventure(adventure_id: str,adventure: Adventure):
+    return {"adventure_id": adventure_id,
+            "adventure name": adventure.name}
+
