dd028d33a14ae5d002358cff400f9d4dc15a32a5
Rati Bakhtadze <ratibakhtadze@gmail.com>
2023-04-21 14:43:41 +0400

    adjusting for the server

--- services/backend/main.py
+++ services/backend/main.py
@@ -5,3 +5,6 @@ from app import create_app # for the deta.space use only app without .
 
 app = create_app()
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
